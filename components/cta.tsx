c9b5d1d955331e53450f12534afb8e7d82456aef
Tayyab Sohail <m.tayyabsohail614@gmail.com>
2025-05-03 17:35:53 +0500

    Initial commit

---
+++ components/cta.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import BlurredShape from "@/public/images/blurred-shape.svg";
+
+export default function Cta() {
+  return (
+    <section className="relative overflow-hidden">
+      <div
+        className="pointer-events-none absolute bottom-0 left-1/2 -z-10 -mb-24 ml-20 -translate-x-1/2"
+        aria-hidden="true"
+      >
+        <Image
+          className="max-w-none"
+          src={BlurredShape}
+          width={760}
+          height={668}
+          alt="Blurred shape"
+        />
+      </div>
+      <div className="max-w6xl mx-auto px-4 sm:px-6">
+        <div className="bg-linear-to-r from-transparent via-gray-800/50 py-12 md:py-20">
+          <div className="mx-auto max-w-3xl text-center">
+            <h2
+              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-8 font-nacelle text-3xl font-semibold text-transparent md:text-4xl"
+              data-aos="fade-up"
+            >
+              Join the cloud-first task management experience{" "}
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
